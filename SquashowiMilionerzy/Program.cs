ae9f9be16215bd5ce499346be3e346d5fb151d8e
SquashFish <tyma.konrad@gmail.com>
2020-10-19 16:30:24 +0200

    Intro added. New questions added to json file.

--- SquashowiMilionerzy/Program.cs
+++ SquashowiMilionerzy/Program.cs
@@ -10,16 +10,26 @@ namespace SquashowiMilionerzy
     {
         static void Main(string[] args)
         {
-            var myJsonString = File.ReadAllText("myfile.json");
+            const string Intro = @"WITAJ W SQUASHOWYCH MILIONERACH
+MASZ SZANSĘ PRZETESTOWAĆ SWOJĄ WIEDZE NA TEMAT SQUASHA
+PODAJ JEDNĄ PRAWIDŁOWĄ ODPOWIEDŹ SPOŚRÓD CZTERACH OPCJI: A, B, C i D
+POWODZENIA";
+
+            Console.WriteLine(Intro);
+
+            var myJsonString = File.ReadAllText("SquashQuestions.json");
             var AllQuestions = JsonSerializer.Deserialize<List<List<Question>>>(myJsonString);
             var selectedQuestions = new List<Question>();
             var dice = new Random(); // random number generator
+
             foreach (var questions in AllQuestions)
             {
                 var i = dice.Next(questions.Count); // int i variable to represent a random number
                 selectedQuestions.Add(questions[i]); // addition of a question from AllQuestions to selectedQuestions
             }
+
             var result = true;
+
             foreach (var question in selectedQuestions)
             {
                 if (result == true)

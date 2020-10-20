9b26f75328d99bca27d7c85d8d958276d496dcb9
SquashFish <tyma.konrad@gmail.com>
2020-10-20 19:12:14 +0200

    Changes to how right and wrong answers are displayed in the console.

--- SquashowiMilionerzy/Question.cs
+++ SquashowiMilionerzy/Question.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace SquashowiMilionerzy
 {
@@ -12,23 +13,67 @@ namespace SquashowiMilionerzy
         {
             Console.WriteLine($"Teraz pytanie za: { DifficultyLevel}PLN");
             Console.WriteLine(QuestionText);
-            foreach (string answer in Answers)
+
+            PrintColorfulAnswers(100,100);
+
+        }
+        private void PrintColorfulAnswers(int correctAnswer, int selectedAnswer)
+        {
+            for (int i = 0; i < Answers.Length; i++)
             {
-                Console.WriteLine(answer);
+                if (i == correctAnswer)
+                {
+                    Colorful.Console.WriteLine(Answers[i], Color.Green);
+                }
+                else if (i ==selectedAnswer)
+                {
+                    Colorful.Console.WriteLine(Answers[i], Color.Red);
+                }
+                else
+                {
+                    Colorful.Console.WriteLine(Answers[i]);
+                }
             }
         }
+        private int GetAnswerIndex(char letter)
+        {
+        
+            if(char.ToLower (letter) == 'a')
+            {
+                return 0;
+            }
+            if (char.ToLower(letter) == 'b')
+            {
+                return 1;
+            }
+            if (char.ToLower(letter) == 'c')
+            {
+                return 2;
+            }
+            if (char.ToLower(letter) == 'd')
+            {
+                return 3;
+            }
+            return 999;
+        }
+
 
         private bool CheckAnswer()
         {
             ConsoleReaderConverter reader = new ConsoleReaderConverter();
 
             char answer = reader.ReadChar();
+            var indexNumber = GetAnswerIndex(answer);
+            Console.SetCursorPosition(0, Console.CursorTop - 5);
             if (answer == char.ToLower(CorrectAnswer))
             {
-                Console.WriteLine("To jest poprawana odpowiedź!");
+                PrintColorfulAnswers(indexNumber,indexNumber);
+                Colorful.Console.WriteLine("To jest poprawana odpowiedź!", Color.Green);
                 return true;
             }
-            Console.WriteLine("To jest zła odpowiedź!");
+            var indexRight = GetAnswerIndex(CorrectAnswer);
+            PrintColorfulAnswers(indexRight, indexNumber);
+            Colorful.Console.WriteLine("To jest zła odpowiedź!",Color.Red);
             return false;
         }
         public bool AskQuestion()
